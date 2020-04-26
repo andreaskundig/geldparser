cf1f6d565c76fd78290c2ed0b5f5238ff2c9cd77
andreaskundig <andreas.kuendig@gmx.ch>
2020-04-26 21:43:10 +0200

    printable Transaction

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 #[macro_use]
 extern crate lazy_static;
-extern crate regex;
 extern crate chrono;
+extern crate regex;
+extern crate rust_decimal;
 
+use chrono::NaiveDate;
 use mt940::parse_mt940;
 use mt940::sanitizers;
 use regex::Regex;
+use rust_decimal::Decimal;
 use std::borrow::Cow;
+use std::fmt;
 use std::fs;
-use chrono::NaiveDate;
-use text_io::read;
 
 // fn accounts() -> [std::string::String;3]{
 //     return [String::from("Expenses::Maestro"),
@@ -26,15 +28,6 @@ pub fn run(config: Config) {
     let filename = config.filename;
     println!("{:?}", filename);
 
-    // let mut input = String::new();
-    // match io::stdin().read_line(&mut input) {
-    //     Ok(n) => {
-    //         println!("{} bytes read", n);
-    //         println!("{}", input);
-    //     }
-    //     Err(error) => println!("error: {}", error),
-    // }
-    
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     let sanitized = sanitizers::sanitize(&contents[..]);
@@ -43,70 +36,92 @@ pub fn run(config: Config) {
     let start_date = NaiveDate::from_ymd(2019, 01, 01);
 
     println!("messages {:?}", messages.len());
-    for message in &messages {
-        println!("=======================================");
-        println!("opening balance {:?} lines {:?}",
-                 message.opening_balance.amount, message.statement_lines.len());
-        println!("=======================================");
+    for (index, message) in (&messages).iter().enumerate() {
+        // for message in &messages {
+        if index == 0 {
+            println!(
+                "{} opening balance {:?} lines {:?}",
+                index,
+                message.opening_balance.amount,
+                message.statement_lines.len()
+            );
+        }
+
         //let statement = &message.statement_lines[0];
         for statement in &message.statement_lines {
-            
             let date = statement.entry_date.unwrap_or(statement.value_date);
             if date < start_date {
                 continue;
             }
-            println!(
-                "{} {}\n {:?}",
-                date,
-                statement.supplementary_details.as_ref().unwrap(),
-                statement.amount,
-            );
-            let entry_o = extract_transaction(statement);
-            if entry_o.is_some() {
-                let entry = entry_o.unwrap();
-                println!("recipient: {}", remove_newlines(entry.recipient));
-                println!("account: {}", entry.account);
-            }
-            let owner_info = statement
-                .information_to_account_owner
-                .as_ref()
-                .map(String::as_ref)
-                .unwrap_or("no info");
-            println!("owner info: {}", owner_info);
-            println!("------------------------");
-
-            loop {
-                let input: String = read!("{}\n");
-                println!("next...{}", input);
-                if input.len() == 0 { break; }
-            }
+            println!("{}\n", Transaction::new(&statement));
         }
     }
 }
 
 struct Transaction<'a> {
-    recipient: &'a str,
+    recipient: Recipient<'a>,
+    details: &'a str,
+    info_to_owner: &'a str,
+    date: &'a NaiveDate,
+    amount: &'a Decimal,
+}
+
+struct Recipient<'a> {
+    name: &'a str,
     account: &'a str,
 }
 
-fn extract_transaction(statement: &mt940::StatementLine) -> Option<Transaction> {
-    let owner_info = extract_owner_info(statement);
-    let _extractors: Vec<fn(&str) -> Option<Transaction>> = vec![
+impl<'a> Transaction<'a> {
+    fn new(statement: &'a mt940::StatementLine) -> Transaction<'a> {
+        let recipient = extract_recipient(statement);
+        let entry_date = statement.entry_date.as_ref();
+        let date = entry_date.unwrap_or(&statement.value_date);
+        let info_to_owner = extract_info_to_owner(statement);
+        Transaction {
+            date: date,
+            amount: &statement.amount,
+            recipient: recipient,
+            details: statement.supplementary_details.as_ref().unwrap(),
+            info_to_owner: info_to_owner.as_ref().unwrap(),
+        }
+    }
+}
+
+impl<'a> fmt::Display for Transaction<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} {}\n  ; {}\n  ; {}\n  {}             {:?}\n  Assets::Checking",
+            self.date,
+            remove_newlines(self.recipient.name),
+            self.details,
+            remove_newlines(self.info_to_owner),
+            self.recipient.account,
+            self.amount
+        )
+    }
+}
+
+fn extract_recipient(statement: &mt940::StatementLine) -> Recipient {
+    let owner_info = extract_info_to_owner(statement);
+    let _extractors: Vec<fn(&str) -> Option<Recipient>> = vec![
         extract_maestro_transaction,
         extract_sig_transaction,
         extract_rest_transaction,
     ];
 
-    owner_info.and_then({ |oi| _extractors.iter().find_map({ |f| f(oi) }) })
+    owner_info
+        .and_then({ |oi| _extractors.iter().find_map({ |f| f(oi) }) })
+        .unwrap()
 }
 
-fn extract_owner_info(statement: &mt940::StatementLine) -> Option<&str> {
+fn extract_info_to_owner(statement: &mt940::StatementLine) -> Option<&str> {
     let op_string_ref: Option<&String> = statement.information_to_account_owner.as_ref();
     // https://stackoverflow.com/questions/31233938/converting-from-optionstring-to-optionstr
     op_string_ref.map(String::as_str)
 }
 
-fn extract_maestro_transaction(owner_info: &str) -> Option<Transaction> {
+fn extract_maestro_transaction(owner_info: &str) -> Option<Recipient> {
     // https://rust-lang-nursery.github.io/rust-cookbook/text/regex.html
     lazy_static! {
         static ref MAESTRO: Regex =
@@ -114,22 +129,21 @@ fn extract_maestro_transaction(owner_info: &str) -> Option<Transaction> {
     }
     let c = MAESTRO.captures(owner_info);
     c.and_then(|cap| {
-        cap.get(1).map(|m| Transaction {
-            recipient: m.as_str(),
+        cap.get(1).map(|m| Recipient {
+            name: m.as_str(),
             account: "Expenses::Maestro",
         })
     })
 }
 
-fn extract_sig_transaction(owner_info: &str) -> Option<Transaction> {
-    // https://rust-lang-nursery.github.io/rust-cookbook/text/regex.html
+fn extract_sig_transaction(owner_info: &str) -> Option<Recipient> {
     lazy_static! {
         static ref SIG: Regex = Regex::new(r"Services Industriels de Geneve").unwrap();
     }
 
     if SIG.is_match(owner_info) {
-        Some(Transaction {
-            recipient: "Services Industriels de Geneve",
+        Some(Recipient {
+            name: "Services Industriels de Geneve",
             account: "Expenses::Appartment::Electricity",
         })
     } else {
@@ -137,16 +151,19 @@ fn extract_sig_transaction(owner_info: &str) -> Option<Transaction> {
     }
 }
 
-fn extract_rest_transaction(owner_info: &str) -> Option<Transaction> {
-    Some(Transaction {
-        recipient: owner_info,
+fn extract_rest_transaction(owner_info: &str) -> Option<Recipient> {
+    Some(Recipient {
+        name: owner_info,
         account: "Expenses::Rest",
     })
 }
 
-//TODO understand cow
+/* Cow
+In Rust, the abbreviation “Cow” stands for “clone on write”.
+ It is an enum with two states: Borrowed and Owned. This means you can use it to abstract over whether you own the data or just have a reference to it. This is especially useful when you want to return a type from a function that may or may not need to allocate.
+https://deterministic.space/secret-life-of-cows.html
+*/
 fn remove_newlines(text: &str) -> Cow<str> {
-    // https://rust-lang-nursery.github.io/rust-cookbook/text/regex.html
     lazy_static! {
         static ref NEWLINE: Regex = Regex::new(r"\n").unwrap();
     }
