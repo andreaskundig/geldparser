eefb466298c078117abc4ff0fc555c278c59e44c
andreaskundig <andreas.kuendig@gmx.ch>
2020-05-09 22:40:19 +0200

    -i

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,7 +1,12 @@
 extern crate geldparser;
 use geldparser::accounts::ACCOUNTS;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     println!("hello example {}", ACCOUNTS[1]);
     println!("hello example debug {:?}", ACCOUNTS[1]);
+    println!("args {:?}", args);
+    let interactive = args.iter().find(|&arg| &arg == &"-i").is_some();
+    println!("interactive {:?}", interactive);
 }
