b74efb3207dfdd341d49e346a70314f56e84861a
andreaskundig <andreas.kuendig@gmx.ch>
2020-07-05 15:06:39 +0200

    improved ods reader

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,24 +1,40 @@
-use calamine::{Reader, open_workbook, Ods};
-use chrono::NaiveDate;
 use anyhow::{anyhow, Result};
+use calamine::{open_workbook, DataType, Ods, Reader};
+use chrono::NaiveDate;
 
-fn main() -> Result<()>{
+fn extract_date(row: &[DataType]) -> Option<NaiveDate> {
+    row[1]
+        .get_string()
+        .map(|date_str| {
+            NaiveDate::parse_from_str(date_str, "%Y-%m-%d").ok()
+        })
+        .flatten()
+}
+
+fn main() -> Result<()> {
     let path = "../Geld.ods"; // "../Geld-old.xlsx";
-    let mut workbook: Ods<_> = open_workbook(path).expect("Cannot open file");
+    let mut workbook: Ods<_> =
+        open_workbook(path).expect("Cannot open file");
     let names = workbook.sheet_names();
     println!("sheets {:?}", names);
-    let range = workbook.worksheet_range("Ausgabe")
+
+    let start_date = NaiveDate::from_ymd(2017, 12, 31);
+    let range = workbook
+        .worksheet_range("Ausgabe")
         .ok_or(anyhow!("Cannot find 'Ausgabe'"))??;
-    for (i, row) in range.rows().skip(1).enumerate() {
-        
-        let date_str = row[1].get_string().ok_or(anyhow!("no date"))?;
-        let date = NaiveDate::parse_from_str(date_str, "%Y-%m-%d")?;
-        let amount = row[4].get_float().ok_or(anyhow!("no amount"))?;
-        let desc  = row[7].get_string().unwrap_or("");
-        println!("{:?} {:?} {:?}", date , amount, desc);
-        if i>10{
-            break;
-        }
-    }
-    Ok(())
+    range
+        .rows()
+        .skip(1)
+        .filter(|row| {
+            let date_o = extract_date(row);
+            date_o.map(|date| date >= start_date).unwrap_or(false)
+        })
+        .take(10)
+        .map(|row| -> Result<()> {
+            let date = extract_date(row).ok_or(anyhow!("no date"))?;
+            let amount = row[4].get_float().ok_or(anyhow!("no amount"))?;
+            let desc = row[7].get_string().unwrap_or("");
+            println!("{:?} {:?} {:?}", date, amount, desc);
+            Ok(())
+        }).collect()
 }
